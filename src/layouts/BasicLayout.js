c925a31a3e65018c49ab6e9859812adb0ea20042
Jaych Su <hi.jaychsu@gmail.com>
2020-09-15 23:21:43 +0800

    Add watermark for all pages

--- src/layouts/BasicLayout.js
+++ src/layouts/BasicLayout.js
@@ -18,6 +18,13 @@ const styles = StyleSheet.create({
     marginTop: -90,
     marginBottom: 10,
   },
+  watermark: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+  },
 })
 
 const BasicLayout = ({
@@ -25,6 +32,7 @@ const BasicLayout = ({
 }) => (
   <Page size="A4" style={styles.doc}>
     <Image fixed src="file://images/page-header.png" style={styles.pageHeader} />
+    <Image fixed src="file://images/watermark.png" style={styles.watermark} />
     { children }
   </Page>
 )
